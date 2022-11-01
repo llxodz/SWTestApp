f800e76893890fba808ac24cc172c6545ee9b8d6
Ilya Gavrilov <llxodz@gmail.com>
2022-11-01 21:35:20 +0300

    Initial Commit

---
+++ SWTestApp/SWTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWTestAppApp.swift
+//  SWTestApp
+//
+//  Created by Ilya Gavrilov on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SWTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
